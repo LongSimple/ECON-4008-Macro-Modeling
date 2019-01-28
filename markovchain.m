9d1311560c1ee4a1e302386088d9647abc995ff6
LongSimple <LongSimple@users.noreply.github.com>
2019-01-27 21:15:47 -0500

    Added notes

--- markovchain.m
+++ markovchain.m
@@ -1,13 +1,13 @@
 clear; close all; clc;
-alpha=.66; %this is working now
+alpha=.66;
 rho=.909;
 sigmasquaredsube=0.014;
-errorterm=0;
-z=100;
-w=30;
+errorterm=0;% need to determine error term
+z=100; %need to determine first z interval
+w=30; %need to determine wage rate
 productivityshock = @(x)x^rho+(exp(1)^(errorterm));
 ndividendfunction = @(x)-((z*(x^alpha)-(w*x))); %x= number of workers
 zprime=productivityshock(z);%calculate z in next state
-n3= fminbnd(ndividendfunction,1,100000000); %minimize negative dividend function
+n3= fminbnd(ndividendfunction,1,1000000000000000); %(minimize negative dividend function)=(maximize dividend function), might want to make upper bound and lower bound variable?
 div2 = (z*(n3^alpha))-(w*n3);
 Y=z*(n3^alpha);
