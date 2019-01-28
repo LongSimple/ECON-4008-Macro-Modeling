364591167fd2c72853dea8ce83e95dfea6aaca21
LongSimple <LongSimple@users.noreply.github.com>
2019-01-27 21:13:00 -0500

    Fixed maximization logic

--- markovchain.m
+++ markovchain.m
@@ -6,8 +6,8 @@ errorterm=0;
 z=100;
 w=30;
 productivityshock = @(x)x^rho+(exp(1)^(errorterm));
-dividendfunction = @(x)(w*x)-(z*(x^alpha)); %x= number of workers
+ndividendfunction = @(x)-((z*(x^alpha)-(w*x))); %x= number of workers
 zprime=productivityshock(z);%calculate z in next state
-n3= fminbnd(dividendfunction,1,100000000);
+n3= fminbnd(ndividendfunction,1,100000000); %minimize negative dividend function
 div2 = (z*(n3^alpha))-(w*n3);
 Y=z*(n3^alpha);
