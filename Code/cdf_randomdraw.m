5400c2178cf0abd0390a7557871a2c7397c96848
LongSimple <LongSimple@users.noreply.github.com>
2019-03-07 16:16:42 -0500

    changes

--- Code/cdf_randomdraw.m
+++ Code/cdf_randomdraw.m
@@ -1,10 +1,8 @@
-function [sample,zIdx] = cdf_randomdraw(cdf,x_values)
+function [sample,index] = cdf_randomdraw(cdf,x_values)
 rand('seed',12345);
 % init random draw
-x = x_values;
-Cx = cdf;
 z = rand;
 %draw sample
-zIdx = min(find(Cx>z));
-sample = x(zIdx);
+index = min(find(cdf>z));
+sample = x_values(index);
 end
